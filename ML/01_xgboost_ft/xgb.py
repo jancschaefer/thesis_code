11c11f0a1fba79787c8dfce7ec0bd825de61c603
schaefej <jan.schaefer@henkel.com>
2018-05-07 21:16:37 +0200

    XGBoost 002

---
+++ ML/01_xgboost_ft/xgb.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# %% imports
+
+import sys,os
+import pyarrow.parquet as pq
+import pandas as pd
+import multiprocessing
+import numpy as np
+
+print(sys.path)
+print(os.getcwd())
+
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# %% Logging Setup
+
+try:
+	from Code.dolog import logger
+	from Code.environment import filePath
+except:
+	try:
+		from dolog import logger
+		from environment import filePath
+	except:
+		sys.exit("Could not import necessary Code blocks.")
+
+logger.info('WD is set to   ' + filePath)
+dataPath = (filePath + '/02_Data/')
+logger.info('Writing to	 ' + dataPath)
+
+# %% Import Data
+
+logger.info('Reading Parquet File')
+data = pq.read_table(dataPath + '/converted.ft.dropped.parquet').to_pandas()
+
+# %% reduce data for testing purposes
+
+data = data.sample(500)
+
+
+# %% split data into X and y
+X = data.loc[:, data.columns != 'NACE_primarycode'].copy()
+Y = data.loc[:, data.columns == 'NACE_primarycode'].copy()
+
+# %% category encoding
+Y["NACE_primarycode"] = Y["NACE_primarycode"].astype('int').astype('category')
+
+X["Country_Iso"] = X["Country_Iso"].astype('category')#.cat.codes
+X["Bvd_Independence"] = X["Bvd_Independence"].astype('category')#.cat.codes
+
+# create dummies
+XD = pd.get_dummies(X,prefix=["CI","BVD"],prefix_sep="_",columns=['Country_Iso','Bvd_Independence'],dummy_na=True)
+
+
+# %% train/test split
+seed = 7
+test_size = 0.33
+X_train, X_test, y_train, y_test = train_test_split(XD, Y, test_size=test_size, random_state=seed)
+eval_set  = [(X_train,X_test), (y_train,y_test)]
+
+# %% training xgboost
+
+# fit model no training data
+model = XGBClassifier(
+	silent=False,
+	verbose_eval=True,
+	max_depth=25,
+	nthread=(multiprocessing.cpu_count()-1),
+	n_estimators=50,
+	eval_set=eval_set
+)
+model.fit(X_train, np.ravel(y_train,order="C"))
+
+print(model)
+
+# %% make predictions for test data
+y_pred = model.predict(X_test)
+predictions = [round(value) for value in y_pred]
+
+
+# %% evaluate predictions
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy: %.2f%%" % (accuracy * 100.0))
---
+++ TextRep/FastText/fastText.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+# %% Logging Setup
+
+try:
+	from Code.dolog import logger
+	from Code.environment import filePath
+except:
+	try:
+		from dolog import logger
+		from environment import filePath
+	except:
+		sys.exit("Could not import necessary Code blocks.")
+
+logger.info('WD is set to   ' + filePath)
+dataPath = (filePath + '/02_Data/')
+logger.info('Writing to	 ' + dataPath)
+
+# %% Import Data
+## Note: Not necessary. Uses sentences.csv from the word2vec model
+
+# %% Train Fasttext
+
+from gensim.models.fasttext import FastText as FT_gensim
+from gensim.models.word2vec import LineSentence
+
+sentences = LineSentence(dataPath + 'sentences.csv')
+
+model_gensim = FT_gensim(size=300)
+
+# build the vocabulary
+model_gensim.build_vocab(sentences)
+
+# train the model
+model_gensim.train(sentences, total_examples=model_gensim.corpus_count, epochs=model_gensim.epochs)
+
+print(model_gensim)
+
+# %% save model
+model_gensim.save(dataPath + 'fasttext.model')
+
+# %% sanity check
+model_gensim.most_similar('cattle')
+model_gensim.most_similar(['super','market'])
+model_gensim.most_similar(['pharma'])
+model_gensim.wv.doesnt_match("pharma medical pharmaceutic cattle".split())
+model_gensim.wv.doesnt_match("pharma medical pharmaceutic".split())
